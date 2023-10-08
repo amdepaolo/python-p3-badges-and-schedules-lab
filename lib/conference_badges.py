e994c94e4b0d28c8db32122dc33a96c129e1c5b6
Adam DePaolo <amdepaolo@gmail.com>
2023-10-08 18:45:57 -0400

    passes tests

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,24 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    badges = [ badge_maker(name) for name in names]
+    return badges
 
 def assign_rooms(names):
-    return None
+    i = 1
+    rooms = []
+    while i < 9:
+        rooms.append(f"Hello, {names[i-1]}! You'll be assigned to room {i}!")
+        i +=1
+    return rooms
 
 def printer(names):
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+    i = 0
+    for badge in badges:
+        print(badge)
+    for room in rooms:
+        print(room)
     return None
